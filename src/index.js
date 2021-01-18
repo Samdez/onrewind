0ee98f8b7abcaa05a77391c036b95af0cee8b18a
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2021-01-18 21:37:44 +0100

    setup apollo and env vars

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { API_URL, HEADER } from './env'
+
+const client = new ApolloClient({
+  uri: API_URL,
+  headers: {
+    "x-account-key": HEADER 
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+client
+  .query({
+    query: gql`
+    query {
+      allVideos {
+        cursor{
+          before
+          after
+        }
+        items {
+          name
+          id
+          poster
+        }
+      }
+    }
+    `
+  })
+  .then(result => console.log(result))
+
