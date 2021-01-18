d5342e62774f686aba57fb9367b1c391c9a1b44c
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2021-01-18 22:00:47 +0100

    added styled components and global style

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import styled  from 'styled-components';
+
+
+const Header = () => {
+  return ( 
+    <HeaderContainer>
+
+    </HeaderContainer>
+   );
+}
+ 
+const HeaderContainer = styled.div`
+  background-color: blue;
+  height: 5vh;
+`
+
+export default Header;
